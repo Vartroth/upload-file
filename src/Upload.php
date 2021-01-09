441feefb1d8faeffbdd336aecf69890ea4c4282e
Adrian Gonzalez <info.agonzalez@gmail.com>
2021-01-09 14:14:52 +0100

    Fix errors

--- src/Upload.php
+++ src/Upload.php
@@ -4,7 +4,7 @@ declare (strict_types = 1);
 namespace Vartroth\UploadFile;
 
 use Vartroth\UploadFile\Entity\File;
-use Vartroth\UploadFile\Entity\UploadedFileType;
+use Vartroth\UploadFile\Entity\FileType;
 use Vartroth\UploadFile\Storage\StorageSystemInterface;
 
 class Upload
@@ -14,16 +14,16 @@ class Upload
     private $path;
 
     public function __construct(
-        UploadedFileType $uploadedfile,
+        FileType $uploadedfile,
         StorageSystemInterface $storageSystem,
         string $path
     ) {
-        $this->file = $uploadedfile->load();
+        $this->file = $uploadedfile;
         $this->storageSystem = $storageSystem;
         $this->path = $path;
     }
 
-    public function save(): File
+    public function save(): FileType
     {
         return $result = $this->storageSystem->process($this->file, $this->path);
     }
